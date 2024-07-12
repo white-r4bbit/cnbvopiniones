4d87142ea271db4808eea48fdd91e889ceeddc91
Gamaliel Sibaja Ibarra <gsibai@desarrollo-ultrasist.com.mx>
2024-07-12 14:07:34 -0600

    ADD Validacion de que solo muestra los archivos que esten false en eliminado

--- src/Cnbv.ConectaProcesos.Opiniones/Sources/Cnbv.ConectaProcesos.Opiniones.Business/OpinionesBusiness.cs
+++ src/Cnbv.ConectaProcesos.Opiniones/Sources/Cnbv.ConectaProcesos.Opiniones.Business/OpinionesBusiness.cs
@@ -176,7 +176,7 @@ namespace Cnbv.ConectaProcesos.Opiniones.Business
               //Archivos asociados al detalle de la opinion.
               var archivosOpinion = await _archivoOpinionRepository.GetFilteredAsync(i => i.IdOpinion == idOpinion);
               archivosOpinion = archivosOpinion.OrderByDescending(a => a.FechaCreacion);
-
+              archivosOpinion = archivosOpinion.Where(a => a.Eliminado == false);
               List<ArchivosModelResponse> archivosRespuesta = new List<ArchivosModelResponse>();
               foreach (var archivoOpinion in archivosOpinion)
               {
@@ -210,6 +210,7 @@ namespace Cnbv.ConectaProcesos.Opiniones.Business
               receptorRespuesta.EstatusSolicitud = opinionReceptor.EstatusSolicitud;
               receptorRespuesta.ComentarioFirmante = opinionReceptor.ComentarioFirmante;
               var archivosReceptor = await _archivoOpinionRepository.GetFilteredAsync(i => i.IdReceptor == idOpinionReceptor);
+              archivosReceptor = archivosReceptor.Where(a => a.Eliminado == false);
 
               archivosReceptor = archivosReceptor.OrderByDescending(a => a.FechaCreacion);
 
